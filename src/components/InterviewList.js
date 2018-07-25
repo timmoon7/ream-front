ce9771d1d0a7095e38e501ba247d1e90dc704320
Tim Moon <tim.moon7@gmail.com>
2018-07-25 12:07:48 +1000

    Change InterviewList and add css for it

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import api from '../api/interviewAPI'
 import ReactTable from 'react-table'
 import { Link } from 'react-router-dom'
+import './InterviewList.css'
 
 class InterviewList extends Component {
 
@@ -40,24 +41,46 @@ class InterviewList extends Component {
         function Expand({_id, intake, year, interviewee, test_score, student_id, 
             scores, outcome, outcome_comment, duration, comment, jr_updated, 
             hubspot_updated, enrolment_confirmed}) {
-            return <div>
-                <p>Intake: {intake}</p> 
-                <p>Year: {year}</p>
-                <p>Email: {interviewee.email}</p>
-                <p>Phone: {interviewee.phone}</p>
-                <p>Technical Skills Average: {}</p>
-                <p>Soft Skills Average: {}</p>
-                <p>Interview Comments: {comment}</p>
-                <p>Interview Duration: {duration} minutes</p>
-                <p>Admission Test Result: {test_score}</p>
-                <p>JR Updated: {jr_updated ? "Yes" : "No"}</p>
-                <p>Hubspot Updated: {hubspot_updated ? "Yes" : "No"}</p>
-                <p>Enrolment Confirmed: {enrolment_confirmed ? "Yes" : "No"}</p>
-                <p>Outcome: {outcome}</p>
-                <p>Outcome Comment: {outcome_comment}</p>
-                <p>Student ID: {student_id}</p>
-                <Link to={{pathname: `/interviews/${_id}/edit`}}>Edit</Link>
+
+            function avg(scores, category) {
+                if (!scores) {
+                    return 0;
+                }
+
+                const sumPerCategory = [0, 0]
+                for (let elm of scores) {
+                    if (elm.category === category) {
+                        sumPerCategory[0] += parseInt(elm.score);
+                        sumPerCategory[1] += 1
+                    }
+    
+                }
+                return sumPerCategory[0] / sumPerCategory[1];
+            }
+
+            return (
+                <div className="expandsection">
+                    <p class="name">Intake: <span class="value"> {intake}</span></p> 
+                    <p class="name">Year: <span class="value"> {year}</span></p>
+                    <p class="name">Email: <span class="value"> {interviewee.email}</span></p>
+                    <p class="name">Phone: <span class="value"> {interviewee.phone}</span></p>
+                    <p class="name">Technical Skills Average: <span class="value"> {avg(scores, "Technical Skills")}</span></p>
+                    <p class="name">Soft Skills Average: <span class="value"> {avg(scores, "Soft Skills")}</span></p>
+                    <p class="name">Total / 10: <span class="value"> {avg(scores, "Technical Skills") + avg(scores, "Soft Skills")}</span></p>
+                    <p class="name">Interview Comments: <span class="value"> {comment}</span></p>
+                    <p class="name">Interview Duration: <span class="value"> {duration} minutes</span></p>
+                    <p class="name">Admission Test Result: <span class="value"> {test_score}</span></p>
+                    <p class="name">JR Updated: <span class="value"> {jr_updated ? "Yes" : "No"}</span></p>
+                    <p class="name">Hubspot Updated: <span class="value"> {hubspot_updated ? "Yes" : "No"}</span></p>
+                    <p class="name">Enrolment Confirmed: <span class="value"> {enrolment_confirmed ? "Yes" : "No"}</span></p>
+                    <p class="name">Outcome: <span class="value"> {outcome}</span></p>
+                    <p class="name">Outcome Comment: <span class="value"> {outcome_comment}</span></p>
+                    <p class="name">Student ID: <span class="value"> {student_id}</span></p>
+                    <button>
+                        <Link to={{pathname: `/interviews/${_id}/edit`}}>Start / Edit Interview</Link>
+                    </button>
                 </div>  
+            )
         }
       
         return (
@@ -87,10 +110,20 @@ class InterviewList extends Component {
                             accessor: "interviewer"
                         },
                         {
-                            Header: "Status",
+                            Header: "Outcome",
                             accessor: "outcome"
+                        },
+                        {
+                            Header: "Student_ID",
+                            accessor: "student_id"
                         }
                     ]}
+                    
+                    sorted={[{
+                        id: 'date_time',
+                        desc: true
+                      }]}
+
                     SubComponent={row => {
 
                         return (
